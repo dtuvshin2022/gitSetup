d1152f512e2ac4251f90d8a3bdb55aaee39cedc1
David Tuvshin <dtuvshin2022@208-Engineering-6.local>
2019-03-22 11:58:07 -0700

    Initial Commit

---
+++ gitSetupTests/gitSetupTests.swift
@@ -0,0 +1,34 @@
+//
+//  gitSetupTests.swift
+//  gitSetupTests
+//
+//  Created by David Tuvshin on 3/22/19.
+//  Copyright © 2019 David Tuvshin. All rights reserved.
+//
+
+import XCTest
+@testable import gitSetup
+
+class gitSetupTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
