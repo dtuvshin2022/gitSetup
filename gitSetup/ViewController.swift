bfa4dcb737d6c978f071dbb65e183d828a65091e
David Tuvshin <dtuvshin2022@steam-10.local>
2019-04-03 11:26:21 -0700

    yooo!

--- gitSetup/ViewController.swift
+++ gitSetup/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var yo: UILabel!
+    @IBOutlet weak var yooo: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
